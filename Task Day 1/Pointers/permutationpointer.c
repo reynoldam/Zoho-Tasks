831ef1590b29f262bebdf2d1c1cec2c48eb502ff
Reynold Amrith <102484405+reynoldam@users.noreply.github.com>
2022-04-05 10:39:53 +0530

    Create permutationpointer.c

---
+++ Task Day 1/Pointers/permutationpointer.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#define length 100
+
+void permu(char *ch,int x,int y);
+void swap(char *a,char *b);
+
+void (*p)(char *,int ,int);
+void (*s)(char *,char *);
+
+int main()
+{
+    char str[length];
+    int num;
+    
+    printf("Enter a String : ");
+    scanf("%s",str);
+    
+    num = strlen(str);
+    p = permu;
+    p(str,0,num-1);
+    
+    return 0;
+}
+
+void permu(char *ch,int x,int y)
+{
+    int i;
+    s = swap;
+    
+    if(x == y)
+    {
+        printf("%s\n",ch);
+        
+    }
+    else
+    {
+        for(i = x; i<= y; i++)
+        {
+            s((ch+x), (ch+i));
+            p(ch, (x+1), y);
+            s((ch+x), (ch+i));
+        }
+    }
+    
+}
+
+
+void swap(char *a,char *b)
+
+{
+    char val;
+    val = *a;
+    *a = *b;
+    *b = val;
+}
