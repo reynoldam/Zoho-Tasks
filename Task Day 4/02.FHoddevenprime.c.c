878825a8750ae961b81ff0b226cded7a6e4d1830
Reynold Amrith <102484405+reynoldam@users.noreply.github.com>
2022-04-06 18:01:57 +0530

    Update 02.FHoddevenprime.c.c

--- Task Day 4/02.FHoddevenprime.c.c
+++ Task Day 4/02.FHoddevenprime.c.c
@@ -1,84 +1,63 @@
 #include<stdio.h>
 #include<stdlib.h>
-int even(int num);
-int prime(int num);
-void fil();
 
-void (*p)();
-
-void main()
-{
-    p = fil;
-    p();
-}
-
-int even(int num)
-{
-    if(num % 2 == 0)
-    {
-        return num;
-    }
-    else
-    {
-        return 1;
-    }
-}
-
-int prime(int num)
-{
-    if(num < 0)
-    {
-        return 0;
-    }
-    for(int i = 2; i<=num/2 ;i++)
-    {
-        if(num % i == 0)
-        {
-            return 0;
-        }
-    }
-    return 1;
-}
-
-void fil()
+int main()
 
 {
     FILE *finput;
     FILE *fodd;
     FILE *feven;
     FILE *fprime;
-    
+
     int numb;
     finput = fopen("Number.txt","r");
     feven = fopen("Even.txt","w");
     fodd = fopen("Odd.txt","w");
     fprime = fopen("Prime.txt","w");
-  
-    
-    
+
+
     if( finput == NULL || fodd == NULL || feven == NULL || fprime == NULL)
     {
         printf("File does not exist");
         exit(1);
     }
+    printf("File opened successfully\n\n");
+
     while(fscanf(finput,"%d",&numb) != -1)
     {
-        if(prime(numb))
+        if(numb < 0)
         {
+            return 0;
+
+            for (int i = 2;i<=numb/2;i++)
+            {
+                if (numb % i == 0)
+                {
+                    return 0;
+                }
+            }
+
+            return 1;
+
             fprintf(fprime,"%d",numb);
         }
-        else if(even(numb))
+
+        else if(numb % 2 == 0)
         {
             fprintf(feven,"%d",numb);
         }
+
         else
         {
             fprintf(fodd,"%d",numb);
         }
     }
-    
+
     fclose(finput);
     fclose(fprime);
     fclose(feven);
     fclose(fodd);
+
+    return 0;
+
 }
