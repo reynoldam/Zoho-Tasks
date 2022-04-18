2feb016b0e384ceb425a6fc2b79dcac0898c4460
Reynold Amrith <102484405+reynoldam@users.noreply.github.com>
2022-04-18 09:39:08 +0530

    Create largest Number.c

---
+++ C Programming Tasks/largest Number.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void largest(int num1, int num2, int num3);
+
+int main()
+{
+   int num1, num2, num3;
+   
+   printf("\n Enter three Numbers : ");
+   scanf("%d %d %d",&num1, &num2, &num3);
+   
+   largest(num1, num2, num3);
+   
+   return 0;
+}
+void largest(int num1, int num2, int num3)
+{
+    if (num1 > num2 && num1 > num3)
+    {
+        printf("%d is the largest Number", num1);
+        
+    }
+    else if(num2 > num3)
+    {
+       printf("%d is the largest Number", num2);
+    }
+   else\
+   {
+       printf("%d is the largest Number",num3);
+   }
+}
