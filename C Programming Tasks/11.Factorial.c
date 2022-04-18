772271bd6c87b4b0f2a18c94cc50445fc56560f4
Reynold Amrith <102484405+reynoldam@users.noreply.github.com>
2022-04-18 09:46:42 +0530

    Create 11.Factorial.c

---
+++ C Programming Tasks/11.Factorial.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+long int Factorial(int);
+
+int main()
+{
+    int n;
+    
+    printf("\n Enter Factorial Number : ");
+    scanf("%d", &n);
+    
+    printf("Factorial of %d = %ld", n, Factorial(n));
+    
+    return 0;
+}
+
+long int Factorial(int n){
+    
+    if(n>1){
+        return n*Factorial(n-1);
+    }
+    else{
+        return 1;
+    }
+}
