b628479ed1928b485aed74760a24956d71672948
Reynold Amrith <102484405+reynoldam@users.noreply.github.com>
2022-04-18 12:19:19 +0530

    Update 19.Array Pointers.c

--- C Programming Tasks/19.Array Pointers.c
+++ C Programming Tasks/19.Array Pointers.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
-
+#define len 50
 int main() 
 {
-    int n, data[20];
+    int n, data[len];
     
     printf("\n Enter the Number of Elements : ");
     scanf("%d", &n);
     
-    printf("\n Enter %d Array elements : \n", n);
+    printf("\nEnter Array elements : \n");
     for (int i = 0; i < n; ++i)
+    {
         scanf("%d", data + i);
+    }
 
-    printf("\n Array Elements : \n");
+    
+    printf("\n Array Elements :\n");
+    
+    printf("[");
+    
     for (int i = 0; i < n; ++i)
-        printf("%d\n", *(data + i));
+    {    
+        printf("\t%d\t", *(data + i));
+    }
+    
+    printf("]");
+    
     return 0;
 }
