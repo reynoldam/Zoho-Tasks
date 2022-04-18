f63e13914983668f12e74711709404eac396286c
Reynold Amrith <102484405+reynoldam@users.noreply.github.com>
2022-04-18 09:47:58 +0530

    Create 12.Array.c

---
+++ C Programming Tasks/12.Array.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#define LENGTH 20
+int printArr(int arr[] ,int n);
+
+int main()
+{ 
+    int arr[LENGTH];
+    int n;
+    
+    printf("\n Enter the number of elements : ");
+    scanf("%d",&n);
+    
+    for(int i=0;i<n;i++)
+    {
+        printf("\n Enter the number %d : ", i+1);
+        scanf("%d", &arr[i]);
+    }
+    printArr(arr,n);
+    
+    return 0;
+}
+int printArr(int arr[],int n){
+
+    printf("Print Entered Array Values are: \n");
+    printf("[");
+    
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    
+    printf("]");
+}
