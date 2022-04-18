790f2c299eeaa9e34bbf33cf72c0f06ed4f7ffd4
Reynold Amrith <102484405+reynoldam@users.noreply.github.com>
2022-04-18 09:50:10 +0530

    Create 15.Find.c

---
+++ C Programming Tasks/15.Find.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#define LENGTH 20
+int find(int arr[] ,int n);
+
+int main()
+{ 
+    int arr[LENGTH];
+    int n;
+    
+    printf("\n Enter the number of elements : ");
+    scanf("%d",&n);
+    
+    for(int i=0;i<n;i++)
+    {
+        printf("\n Enter the number %d : ", i+1);
+        scanf("%d", &arr[i]);
+    }
+    find(arr,n);
+    
+    return 0;
+}
+int find(int arr[],int n){
+
+    int f, flag = 0;
+    
+    printf("\n Enter the number to find in Array : ");
+    scanf("%d", &f);
+    
+    for(int i = 0; i < n;i++){
+        if (arr[i] == f)
+        {
+            printf("\n The number found in pos %d", i+1);
+            flag = 1;
+            break;
+        }
+    }
+    
+    if (flag == 0)
+    {
+        printf("\n NUmber not found");
+    }
+    
+}
