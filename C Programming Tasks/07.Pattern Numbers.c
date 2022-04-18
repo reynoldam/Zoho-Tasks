dedcfc54b0b3e58550b78e3bf4d98a31829427a1
Reynold Amrith <102484405+reynoldam@users.noreply.github.com>
2022-04-18 09:44:19 +0530

    Create 07.Pattern Numbers.c

---
+++ C Programming Tasks/07.Pattern Numbers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j, n;
+    
+    printf("\n Enter the number of lines : ");
+    scanf("%d", &n);
+    
+    printf("\n");
+    for(i = 1; i < n+1; i++)
+    {
+        for(j = 0; j < i; j++)
+        {
+            printf(" %d ", j+1);
+        }
+        printf("\n");
+    }
+    
+    printf("\n ================ \n\n");
+    for(i = n; i > 0; i--)
+    {
+        for(j = 1; j < i+1; j++)
+        {
+            printf(" %d ", j);
+        }
+        printf("\n");
+    }
+    
+
+    return 0;
+}
