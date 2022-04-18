11ed409eb6032862dafb280678a559fd44c376ea
Reynold Amrith <102484405+reynoldam@users.noreply.github.com>
2022-04-18 10:22:55 +0530

    Update 03.Grade.c

--- C Programming Tasks/03.Grade.c
+++ C Programming Tasks/03.Grade.c
@@ -1,40 +1,67 @@
 #include<stdio.h>
 
+void grade(int num,int sum);
 
 int main()
-{
-    int num = 0, total=500, sum=0;
-    float per;
 
-    printf("\n Enter Marks of 5 Subjects out of 100 : ");
+{
+    int num = 0;
+	int sum=0;
+    
+
+    printf("\n Enter Marks of 5 Subjects out of 100 :\n");
 
     for(int i = 0; i < 5; i++)
-    {
+
+   {
         printf("\n\n Enter Mark %d : ", i+1);
         scanf("%d", &num);
         sum += num;
     }
 
     printf("\n Sum of 5 Subjects is: %d\n", sum);
+    
+	grade(num,sum);
+	
+	return 0;
+}
 
-    per = (sum * 100/total) ;
+void grade(int num,int sum)
 
-    printf("\n Percentage is: %f",per);
+{
+
+	float per;
+	
+	int total=500;
+	
+    per = (sum * 100/total);
+
+    printf("\n Percentage is: %.2f",per);
     printf("\n\n Grade : ");
-    if(per>80){
+    
+    if(per>80)
+    
+	{
         printf("A+");
     }
-    else if(per>=65 && per<=80){
+    
+    else if(per>=65 && per<=80)
+	{
         printf("A");
     }
-    else if(per>=50 && per<=65){
+    
+    else if(per>=50 && per<=65)
+	{
         printf("B");
     }
-    else if(per>=42 && per<=50){
+    
+    else if(per>=42 && per<=50)
+	{
         printf("C");
     }
-    else if(per<42){
+    
+    else if(per<42)
+	{
         printf("FAIL");
     }
-    return 0;
 }
