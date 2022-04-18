5074df6f839fbdfd647503538f6fec6fe56fa26d
Reynold Amrith <102484405+reynoldam@users.noreply.github.com>
2022-04-18 09:38:17 +0530

    Add files via upload

---
+++ C Programming Tasks/Leap Year.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int year;
+    
+    printf("\n Enter a Year : ");
+    scanf("%d", &year);
+    
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+    {
+        printf("\n Leap Year.");
+    }
+    else
+    {
+        printf("\n Not a leap Year.");
+    }
+    return 0;
+}
