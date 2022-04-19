df03fdececf0cba8ddd10f8abad30262595ce48c
Reynold Amrith <102484405+reynoldam@users.noreply.github.com>
2022-04-19 11:50:22 +0530

    Update 18.Maximum.c

--- C Programming Tasks/18.Maximum.c
+++ C Programming Tasks/18.Maximum.c
@@ -4,24 +4,28 @@ int CheckMax(int *,int *);
 
 int main()
 {
-    int f, s, *ptr,*ptr1;
+    int first, second, *ptr,*ptr1;
     
     printf("\n Enter Two Numbers : ");
-    scanf("%d %d",&f, &s);
+    scanf("%d %d",&first, &second);
     
-    ptr = &f;
-    ptr1 = &s;
+    ptr = &first;
+    ptr1 = &second;
     
     CheckMax(ptr, ptr1);
     
     return 0;
 }
+
 int CheckMax(int *ptr,int *ptr1)
+
 {
-    if(*ptr > *ptr1){
+    if(*ptr > *ptr1)
+    {
         printf(" %d is Largest",*ptr);
     }
-    else{
+    else
+    {
         printf(" %d is Largest",*ptr1);
     }
     return 0;
