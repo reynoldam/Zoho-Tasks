895b3f395f2c3881e9d3c7dc1ebc7772f273761d
Reynold Amrith <102484405+reynoldam@users.noreply.github.com>
2022-04-19 11:48:46 +0530

    Update 16.Add Two Matrix.c

--- C Programming Tasks/16.Add Two Matrix.c
+++ C Programming Tasks/16.Add Two Matrix.c
@@ -1,28 +1,40 @@
 #include<stdio.h>
-int main(){
-    int a[10][10],b[10][10],sum[10][10];
+#define len 20
+
+int sum(int r,int c,int a[][],int b[][]);
+
+int main()
+{
+    int a[len][len],b[len][len];
 
     int r,c;
+    
     printf("\n Enter row and col size : ");
     scanf("%d %d",&r,&c);
     
     printf("\n Enter values of Matrix 1 : \n");
-    for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
             scanf("%d",&a[i][j]);
         }
     }
     
     printf("\n Enter values of Matrix 2 : \n");
-    for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
             scanf("%d",&b[i][j]);
         }
     }
     
     printf("\n Matrix 1 : \n");
-    for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
             printf("%3d ",a[i][j]);
         }
         printf("\n");
@@ -30,26 +42,42 @@ int main(){
     
     printf("\n Matrix 2 : \n");
     printf("\n");
-    for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
             printf("%3d ",b[i][j]);
         }
         printf("\n");
     }
     
+    sum(r,c,a,b);
     
-    for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
-            sum[i][j]=a[i][j]+b[i][j];
+    return 0;
+}
+
+int sum(int r,int c,int a[][],int b[][])
+{
+    int sums[len][len];
+    
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            sums[i][j]=a[i][j]+b[i][j];
         }
         
     }
+    
     printf("SUM OF TWO MATRIX : \n");
-    for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
-             printf("%3d ",sum[i][j]);
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+             printf("%3d ",sums[i][j]);
         }
         printf("\n");
     }
+    
     return 0;
 }
