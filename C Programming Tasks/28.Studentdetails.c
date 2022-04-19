05aada51bb454c4632d85253b37599e696535cc0
Reynold Amrith <102484405+reynoldam@users.noreply.github.com>
2022-04-19 13:21:10 +0530

    Update 28.Studentdetails.c

--- C Programming Tasks/28.Studentdetails.c
+++ C Programming Tasks/28.Studentdetails.c
@@ -1,1 +1,68 @@
+#include<stdio.h>
+#define len 20
 
+typedef struct student
+{
+    int id;
+    char firstname[len];
+    char lastname[len];
+    char dept[len];
+    
+}stud;
+
+stud s[len];
+
+void getdetails();
+void studentdetails(int num);
+
+void (*p)();
+
+void main()
+{
+    p = getdetails;
+    p();
+    
+}
+
+void getdetails()
+{
+    int num;
+    
+    printf("Enter the number of students:");
+    scanf("%d",&num);
+    
+    for(int i = 0;i<num;i++)
+    {
+        printf("\nEnter the (Student %d) First name:\t",i+1);
+        scanf("%s",s[i].firstname);
+        printf("\nEnter the (Student %d) Last Name:\t",i+1);
+        scanf("%s",s[i].lastname);
+        printf("\nEnter the (Student %d) Department:\t",i+1);
+        scanf("%s",s[i].dept);
+        printf("\nEnter the (Student %d) Register Number:\t",i+1);
+        scanf("%d",&s[i].id);
+    }
+    
+ studentdetails(num);
+ 
+}
+
+void studentdetails(int num)
+{
+    FILE *fdet;
+    
+    fdet = fopen("Studentdetails.txt","w"); 
+    
+    for(int i = 0;i<num;i++)
+    
+    {
+        fprintf(fdet,"\nStudent %d Details\n",i+1);
+        fprintf(fdet,"\nFirst Name : %s\n",s[i].firstname);
+        fprintf(fdet,"\nLast Name: %s\n",s[i].lastname);
+        fprintf(fdet,"\nDepartment: %s\n",s[i].dept);
+        fprintf(fdet,"\nRegister Number: %d",s[i].id);
+        
+    }
+    fclose(fdet);
+    
+}
